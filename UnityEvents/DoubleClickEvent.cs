0918f4f541088ef40d4945d141e251464c6abbec
Cody <canderson@trideum.com>
2019-09-02 16:13:54 -0500

    DoubleClickEvent

---
+++ UnityEvents/DoubleClickEvent.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+public class DoubleClickEvent : MonoBehaviour, IPointerDownHandler
+{
+    [ReadOnly] public bool clicked = false;
+    public float threshold = .5f;
+
+    public UnityEvent OnDoubleClick;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if(clicked)
+        {
+            OnDoubleClick.Invoke();
+            clicked = false;
+        }
+        else
+        {
+            StartCoroutine(Click());
+        }
+    }
+
+    private IEnumerator Click()
+    {
+        clicked = true;
+        yield return new WaitForSeconds(threshold);
+        clicked = false;
+    }
+}
