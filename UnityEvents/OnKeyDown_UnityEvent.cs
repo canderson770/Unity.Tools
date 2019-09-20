fedf2f50de76a92285338ffb39253390cbbe6876
Cody <canderson@trideum.com>
2019-09-20 16:01:48 -0500

    OnKeyDownUnityEvent

---
+++ UnityEvents/OnKeyDown_UnityEvent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnKeyDown_UnityEvent : MonoBehaviour
+{
+    public KeyCode key;
+    public UnityEvent onKeyDown;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(key))
+        {
+            onKeyDown.Invoke();
+        }
+    }
+}
