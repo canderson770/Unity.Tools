5b7a2761a7b299e2c140c019aee60d93368ccc21
Cody <canderson@trideum.com>
2019-09-26 16:29:24 -0500

    OnVideoEndUnityEvent

---
+++ UnityEvents/OnVideoEndUnityEvent.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.Events;
+
+public class OnVideoEndUnityEvent : MonoBehaviour
+{
+    private VideoPlayer videoPlayer;
+    public UnityEvent onVideoEnd;
+
+    private void Awake()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+    }
+
+    private void OnEnable()
+    {
+        if (videoPlayer != null)
+            videoPlayer.loopPointReached += VideoEnd;
+    }
+    private void OnDisable()
+    {
+        if (videoPlayer != null)
+            videoPlayer.loopPointReached -= VideoEnd;
+    }
+
+    private void VideoEnd(VideoPlayer vp)
+    {
+        onVideoEnd.Invoke();
+    }
+}
