9b1a4191eca5755650b7e053e90b4b5e1881d62b
Cody <canderson@trideum.com>
2019-09-26 16:29:14 -0500

    LoadScene

---
+++ Scripts/LoadScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void LoadSceneIndex(int scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
