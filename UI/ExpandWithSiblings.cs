c96e3944182a59f1c4211dc1a9921d737553f430
Cody <canderson@trideum.com>
2019-10-08 11:11:09 -0500

    improved ExpandWithSiblings script

--- UI/ExpandWithSiblings.cs
+++ UI/ExpandWithSiblings.cs
@@ -21,33 +21,34 @@ public class ExpandWithSiblings : MonoBehaviour
         layoutElement = GetComponent<LayoutElement>();
     }
 
+    //  TODO optimize
     public void Update()
     {
         float leftOffset = Left;
         foreach (var item in left)
         {
-            if (item.gameObject.activeSelf)
+            if (item != null && item.gameObject.activeSelf)
                 leftOffset += item.rect.width;
         }
 
         float rightOffset = Right;
         foreach (var item in right)
         {
-            if (item.gameObject.activeSelf)
+            if (item != null && item.gameObject.activeSelf)
                 rightOffset += item.rect.width;
         }
 
         float topOffset = Top;
         foreach (var item in top)
         {
-            if (item.gameObject.activeSelf)
+            if (item != null && item.gameObject.activeSelf)
                 topOffset += item.rect.height;
         }
 
         float bottomOffset = Bottom;
         foreach (var item in bottom)
         {
-            if (item.gameObject.activeSelf)
+            if (item != null && item.gameObject.activeSelf)
                 bottomOffset += item.rect.height;
         }
 
@@ -57,14 +58,29 @@ public class ExpandWithSiblings : MonoBehaviour
 
         if (vertical != null)
         {
-            RectTools.SetHeight(rt, horizontal.GetComponent<RectTransform>().rect.height - topOffset - bottomOffset);
+            var vertRT = vertical.GetComponent<RectTransform>();
+            if (vertRT != null)
+            {
+                var newHeight = vertRT.rect.height - topOffset - bottomOffset - GetLayoutGroupOffset(vertical);
+
+                if (layoutElement != null)
+                    layoutElement.minHeight = newHeight;
+
+                rt.SetHeight(newHeight);
+            }
         }
         else if (horizontal != null)
         {
-            if (layoutElement != null)
-                layoutElement.minWidth = horizontal.GetComponent<RectTransform>().rect.width - leftOffset - rightOffset;
+            var horizRT = horizontal.GetComponent<RectTransform>();
+            if (horizRT != null)
+            {
+                var newWidth = horizRT.rect.width - leftOffset - rightOffset - GetLayoutGroupOffset(horizontal);
 
-            RectTools.SetWidth(rt, horizontal.GetComponent<RectTransform>().rect.width - leftOffset - rightOffset);
+                if (layoutElement != null)
+                    layoutElement.minWidth = newWidth;
+
+                rt.SetWidth(newWidth);
+            }
         }
         else
         {
@@ -72,10 +88,39 @@ public class ExpandWithSiblings : MonoBehaviour
             rt.anchorMax = Vector2.one;
             rt.pivot = Vector2.one / 2;
 
-            RectTools.SetLeft(rt, leftOffset);
-            RectTools.SetRight(rt, rightOffset);
-            RectTools.SetTop(rt, topOffset);
-            RectTools.SetBottom(rt, bottomOffset);
+            rt.SetLeft(leftOffset);
+            rt.SetRight(rightOffset);
+            rt.SetTop(topOffset);
+            rt.SetBottom(bottomOffset);
         }
+    }
+
+    /// <summary>
+    /// Calculates offset needed to counter layout group's padding and spacing
+    /// </summary>
+    private float GetLayoutGroupOffset(HorizontalOrVerticalLayoutGroup layoutGroup)
+    {
+        float offset = 0;
+
+        if (layoutGroup != null)
+        {
+            if (layoutGroup is HorizontalLayoutGroup)
+            {
+                offset += layoutGroup.padding.left;
+                offset += layoutGroup.padding.right;
+            }
+            else if (layoutGroup is VerticalLayoutGroup)
+            {
+                offset += layoutGroup.padding.top;
+                offset += layoutGroup.padding.bottom;
+            }
+
+            offset += layoutGroup.spacing * top.Count;
+            offset += layoutGroup.spacing * bottom.Count;
+            offset += layoutGroup.spacing * left.Count;
+            offset += layoutGroup.spacing * right.Count;
+        }
+
+        return offset;
     }
 }
