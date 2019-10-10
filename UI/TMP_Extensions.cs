7872becf30c220f4466370874197c0b08e47ee5c
Cody <canderson@trideum.com>
2019-10-10 13:45:43 -0500

    TMP_Extensions

---
+++ UI/TMP_Extensions.cs
@@ -0,0 +1,17 @@
+﻿namespace TMPro
+{
+    public static class TMP_Extensions
+    {
+        public static void SetTextIfNotNull(this TextMeshProUGUI textMeshProGUI, string text)
+        {
+            if (textMeshProGUI != null)
+                textMeshProGUI.SetText(text);
+        }
+
+        public static void SetTextIfNotNull(this TMP_InputField inputField, string text)
+        {
+            if (inputField != null && inputField.text != null)
+                inputField.text = text;
+        }
+    }
+}
