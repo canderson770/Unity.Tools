478ab34404fb2959498baa39368821c5dbe46a3d
Cody Anderson <canderson@trideum.com>
2019-11-07 16:55:10 -0700

    Set TMP color if not null

--- UI/TMP_Extensions.cs
+++ UI/TMP_Extensions.cs
@@ -1,4 +1,6 @@
-﻿namespace TMPro
+﻿using UnityEngine;
+
+namespace TMPro
 {
     public static class TMP_Extensions
     {
@@ -8,10 +10,22 @@
                 textMeshProGUI.SetText(text);
         }
 
+        public static void SetColor(this TextMeshProUGUI textMeshProGUI, Color color)
+        {
+            if (textMeshProGUI != null)
+                textMeshProGUI.color = color;
+        }
+
         public static void SetTextIfNotNull(this TMP_InputField inputField, string text)
         {
             if (inputField != null && inputField.text != null)
                 inputField.text = text;
+        }
+
+        public static void SetColor(this TMP_InputField inputField, Color color)
+        {
+            if (inputField != null && inputField.textComponent != null)
+                inputField.textComponent.color = color;
         }
     }
 }
