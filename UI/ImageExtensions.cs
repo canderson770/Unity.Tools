caee0131401814974036ee54de6f1bfcd7475525
Cody Anderson <canderson@trideum.com>
2019-11-13 20:27:05 -0700

    ImageExtensions

---
+++ UI/ImageExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace UnityEngine.UI
+{
+    public static class ImageExtensions
+    {
+        /// <summary>
+        /// Sets color of image, if image is not null
+        /// </summary>
+        public static void SetColorIfNotNull(this Image image, Color color)
+        {
+            if (image != null)
+                image.color = color;
+        }
+
+        /// <summary>
+        /// Sets sprite of image, if image is not null
+        /// </summary>
+        public static void SetSpriteIfNotNull(this Image image, Sprite sprite)
+        {
+            if (image != null)
+                image.sprite = sprite;
+        }
+    }
+}
