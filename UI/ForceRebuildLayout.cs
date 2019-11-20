027fc9bf79a86b1a19e240719d5ce32481420a95
Cody Anderson <canderson@trideum.com>
2019-11-19 19:29:40 -0700

    ForceRebuildLayout

---
+++ UI/ForceRebuildLayout.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ForceRebuildLayout : MonoBehaviour
+{
+    public bool rebuildOnStart = true;
+
+    private IEnumerator Start()
+    {
+        yield return null;
+
+        if (rebuildOnStart)
+            RebuildLists();
+    }
+
+    /// <summary>
+    /// Force Rebuilds layout
+    /// </summary>
+    public void RebuildLists()
+    {
+        LayoutRebuilder.ForceRebuildLayoutImmediate((RectTransform)transform);
+    }
+}
