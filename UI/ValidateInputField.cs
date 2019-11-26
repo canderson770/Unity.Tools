ce6cc3f63a161c1838e103959ea8066f9bf4a336
Cody Anderson <canderson@trideum.com>
2019-11-26 16:29:54 -0700

    ValidateInputField

---
+++ UI/ValidateInputField.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ValidateInputField : MonoBehaviour
+{
+    private TMP_InputField input;
+
+    public bool isLetter = false;
+    public bool isNumber = false;
+
+    public List<char> possibleChars = new List<char>();
+
+    private void Awake()
+    {
+        input = GetComponent<TMP_InputField>();
+    }
+
+    private void OnEnable()
+    {
+        if (input != null)
+        {
+            input.onValidateInput += Validate;
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (input != null)
+        {
+            input.onValidateInput -= Validate;
+        }
+    }
+
+    private char Validate(string text, int charIndex, char addedChar)
+    {
+        //  if letter
+        if (char.IsLetter(addedChar) && isLetter)
+            return addedChar;
+
+        //  if number
+        if (char.IsDigit(addedChar) && isNumber)
+            return addedChar;
+
+        //  if in list of possible chars
+        foreach (var c in possibleChars)
+        {
+            if (addedChar == c)
+                return addedChar;
+        }
+
+        //  else don't allow
+        return '\0';
+    }
+}
