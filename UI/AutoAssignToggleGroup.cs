9ccef93f71c1b71931be47a37d6065a99e9bc1fb
Cody Anderson <canderson@trideum.com>
2019-12-02 18:09:26 -0700

    MatchScreenSize

---
+++ UI/AutoAssignToggleGroup.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AutoAssignToggleGroup : MonoBehaviour
+{
+    private Toggle[] toggles;
+    private ToggleGroup toggleGroup;
+
+    public bool overrideGroup = true;
+
+    private IEnumerator Start()
+    {
+        toggleGroup = GetComponent<ToggleGroup>();
+
+        if (toggleGroup != null)
+        {
+            yield return null;
+            toggles = GetComponentsInChildren<Toggle>();
+
+            foreach (var toggle in toggles)
+            {
+                if (toggle != null)
+                {
+                    if (toggle.group == null || overrideGroup)
+                    {
+                        toggle.group = toggleGroup;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ UI/MatchScreenSize.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MatchScreenSize : MonoBehaviour
+{
+    private RectTransform rt;
+
+    private void Awake()
+    {
+        rt = (RectTransform)transform;
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(MatchCanvas());
+    }
+
+    private IEnumerator MatchCanvas()
+    {
+        LayoutRebuilder.ForceRebuildLayoutImmediate((RectTransform)transform.parent);
+        yield return null;
+        RectTransform canvas = (RectTransform)transform.root;
+        rt.SetPivot(canvas.pivot);
+        rt.position = canvas.position;
+        rt.SetWidth(canvas.sizeDelta.x);
+        rt.SetHeight(canvas.sizeDelta.y);
+    }
+}
